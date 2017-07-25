1cf377734566630143741706a90c0d112a94e5d1
Jerizon <JesseLowellReyes@gmail.com>
2017-07-24 20:16:57 -0700

    Heroku Redis Deployment

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,7 +2,7 @@ const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
 const config = require('config')['redis'];
 console.log(config);
-const redisClient = require('redis').createClient(config['REDIS_URL']);
+const redisClient = require('redis').createClient(config['REDIS_URL'] || null);
 
 module.exports.verify = (req, res, next) => {
   if (req.isAuthenticated()) {
