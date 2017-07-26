7b8cd6d9edf0fe9541f766581a9d67de485616bc
Julia Wong <juliawong05@gmail.com>
2017-07-26 12:30:36 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = require('./app');
 const db = require('../db');
 const port = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(port, () => {
   console.log('Tessel8 listening on port', port, 'at', timeFormat(new Date()));
 }); 
 
