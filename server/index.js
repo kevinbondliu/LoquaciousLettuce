5df1d82b00a6672a3445c44715d6323f44ce9b01
Kurt Larson <ThalyaFlourishing@gmail.com>
2017-07-25 17:18:34 -0700

    Add time-stamp console message

--- server/index.js
+++ server/index.js
@@ -4,5 +4,17 @@ const db = require('../db');
 const PORT = process.env.port || 3000;
 
 app.listen(PORT, () => {
-  console.log('Tessel8 listening on port 3000!');
+  console.log('Tessel8 listening on port ', PORT, ' at ', timeFormat(new Date()));
 }); 
+
+let timeFormat = function(time) {
+  let hours = zeroes(time.getHours());
+  let minutes = zeroes(time.getMinutes());
+  let seconds = zeroes(time.getSeconds());
+  let timeString = hours + ':' + minutes + ':' + seconds;
+
+  var zeroes = function(number) {
+    return (number < 10) ? '0' + number : number;
+  }
+  return timeString;
+};
