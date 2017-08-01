b0041a0d09a9820089220d08c18bb620f2f394ed
KevinLiu <kevinbondliu@gmail.com>
2017-08-01 15:28:29 -0700

    bug fix

--- client/src/containers/settings.jsx
+++ client/src/containers/settings.jsx
@@ -40,10 +40,14 @@ class Settings extends React.Component {
     return (
       <div>
 <<<<<<< HEAD
+<<<<<<< HEAD
         <img src="assets/pics/logoTest.png" height = "300" width="1100" className ='logo'></img>
 =======
         <img src="assets/pics/logoTest.png" height = "300" width="1100"classname ='logo' align='middle'></img>
 >>>>>>> added better animation + logo
+=======
+        <img src="assets/pics/logoTest.png" height = "300" width="1100" className ='logo'></img>
+>>>>>>> bug fix
         <br />
           <div>
             <div className="settingsPage">Settings Page</div>
